92db8796937a613711f9d5eee74f946abf52b5a8
olejos777 <olejniczakk.maciejj@gmail.com>
2020-06-16 20:23:31 +0200

    12.2 add comments

---
+++ README.md
@@ -0,0 +1,12 @@
+# todolist
+
+# npm install - install all packages configured in package.json
+# npm start - inits "webpack-dev-server --mode development --open --hot", (developer's version of the application)
+# npm run build - inits "webpack --mode production" (production version of the application (stable))
+
+
+styles_bundle_main.css - all styles of the application
+dist/scripts_bundle.js - is a file that joints all JS code of our application which then is converted(translated) by Babel so that it's readable for web browser
+
+.bablerc - is a configuration filr
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const baseConfig = () => ({
-  entry: './src/index.js',
-  output: {
+  entry: './src/index.js',                                                        // all the files are imported to this file
+  output: {                                                                       // localization of production version
     path: path.join(__dirname, 'dist'),
     filename: 'scripts_bundle.js',
   },
@@ -19,7 +19,7 @@ const baseConfig = () => ({
         },
       },
     ],
-  }, 
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -33,14 +33,14 @@ const devConfig = () => ({
       {
         test: /\.(s*)css$/,
         use: [
-          'style-loader', 
+          'style-loader',
           {
             loader: 'css-loader',
             query: {
-              modules: true, 
+              modules: true,
               localIdentName: '[name]_[local]_[hash:base64:5]',
             },
-          }, 
+          },
           'sass-loader',
         ],
       },
@@ -52,22 +52,25 @@ const prodConfig = () => ({
   module: {
     rules: [
       {
-        test:/\.(s*)css$/,
-        use:[
-          MiniCssExtractPlugin.loader,
+        test: /\.(s*)css$/,
+        use: [
+          MiniCssExtractPlugin.loader,                                                                //This plugin extracts CSS into separate files. It creates a CSS file per JS file which contains CSS. It supports On-Demand-Loading of CSS and SourceMaps.
           {
             loader: 'css-loader',
             query: {
-              modules: true, 
+              modules: true,
               localIdentName: '[name]_[local]_[hash:base64:5]',
             },
-          }, 
+          },
           'sass-loader',
         ],
       },
     ],
-  }, 
+  },
   plugins: [
+    // MiniCssExtractPluginsimplifies creation of HTML files to serve your webpack bundles.
+    // This is especially useful for webpack bundles that include a hash in the filename which changes every compilation.
+    // You can either let the plugin generate an HTML file for you, supply your own template using lodash templates, or use your own loader.
     new MiniCssExtractPlugin({
       filename: 'styles_bundle_[name].css',
       chunkFilename: '[id].css',
