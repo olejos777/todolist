748d59b09af9f7f044f7ee85b4f06ffd50b0f94c
olejos777 <olejniczakk.maciejj@gmail.com>
2020-06-17 20:45:20 +0200

    12.5 import pageContents adn listData from dataStore.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './App.scss';  // loaders configured in webpack.config.js in "module" section extract new components with styles
 import List from '../List/List.js';
+import { pageContents, listData } from '../../data/dataStore.js';
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
-        <List                                                                                                                       /* to create a mix of HTML string + styled HTML you just need to close the code into brackets "{}"     every object in array has to posses key property which should be unique in particular array */
-          title={['Things to do', <sup key='1'> soon!</sup>]}
-          image={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <List
+          // spread operator that unpacks object or array. All properties of from dataStore.listData will be assigned to the component List as its properties
+          {...listData}
         >
           <p>I'm going to do nothing and live like a king</p>
         </List>
