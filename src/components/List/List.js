679c35341b4f8601baf0df9bb22a6dd55c12955a
olejos777 <olejniczakk.maciejj@gmail.com>
2020-06-18 20:56:55 +0200

    12.5 add map() method for Column component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -8,6 +8,10 @@ import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
 
+  state = {                                         // only for initial state it's allowed to set state with " = ". In other cases it will be changed by this.setState inherited (odziedziczony) from class React.Component
+    columns: this.props.columns || [],             // it gets properties that are unpacked in App.js from {...listData}. There is no need to import these props again here
+  }
+
   static propTypes = {                             // static means that we are defining static property of this class and the this object will not be available for all instances. It will be defined as "List.propTypes" (class property and not instance property)
     title: PropTypes.node,
     imageSource: PropTypes.string,
@@ -25,16 +29,54 @@ class List extends React.Component {
         <Hero
           titleText={this.props.title}
           imageSource={this.props.image}
-        />                                        {/* titleText - prop name (we choose the name) which is which is provided to component Hero. // this.props - refers to this class instance (App in this case). title - it's name of the prop (we choose the name for the props but has to be the same like in class from which it's imported) */}
+        />                                        {/* titleText - prop name (we choose the name) which is provided to component Hero. // this.props - refers to this class instance (App in this case). title - it's name of the prop (we choose the name for the props but has to be the same like in class from which it's imported) */}
 
         <div className={styles.description}>
-          {ReactHtmlParser(this.props.description)}                   {/* if props are not defined(if they are not named) we can use prop "children" which will refer to children element of this class instance */}
+          {ReactHtmlParser(this.props.description)}  {/* it gets properties that are unpacked in App.js from {...listData}. There is no need to import these props again here */}
         </div>
 
         <div className={styles.columns}>
-          <Column columnTitle='Column 1'/>
-          <Column columnTitle='Column 2'/>
-          <Column columnTitle='Column 3'/>
+          {/* map() method returns converted elements of array this.state.columns - it creates an instance of Column class by using JSX (it assigns proper props from initial array (this.state.columns) to the key and columnProps arguments*/}
+          {this.state.columns.map(({ key, ...columnProps }) => (                  // the "map()" method returns an array with changed content
+            <Column key={key} {...columnProps} />                               // it's mandatory to declare the "key" in JSX for objects/arrays
+          ))}
+
+          {/*explaination of above: for each single column return {key} property assigned to other props (title, icon, cards)
+
+                function(singleColumn){
+                  const key = singleColumn.key;
+
+                  const columnProps = {};
+
+                  for(let propName in singleColumn){
+                    if(propName != 'key'){
+                      columnProps[propName] = singleColumn[propName];
+                    }
+                  }
+
+                  return <Column key={key} {...columnProps} />
+                }
+
+
+                or:
+
+                function(singleColumn){
+                  {key, ...columnProps} = singleColumn;
+
+                  return <Column key={key} {...columnProps} />
+                }
+
+                en zapis oszczędził nam sporo miejsca! Ale skoro tylko raz używamy argumentu singleColumn, to możemy w ogóle go nie nazywać,
+                tylko od razu w deklaracji argumentów użyć wyrażenia {key, ...columnProps}.
+
+                function({key, ...columnProps}){
+                  return <Column key={key} {...columnProps} />
+                }
+
+          */
+          }
+
+
         </div>
       </section>
     );
