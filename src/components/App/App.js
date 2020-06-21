f0a19ccff5aa0403913b2368c14edf062f2dc5cc
olejos777 <olejniczakk.maciejj@gmail.com>
2020-06-21 16:54:47 +0200

    13.3 use propTypes in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import styles from './App.scss';  // loaders configured in webpack.config.js in "module" section extract new components with styles
-import List from '../List/List.js';
-import { pageContents, listData } from '../../data/dataStore.js';
+//import List from '../List/List.js';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+  }
+
+
   render() {
+    const { title, subtitle } = this.props;
+
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <List
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        {/* <List
           // spread operator that unpacks object or array. All properties of from dataStore.listData will be assigned to the component List as its properties
           {...listData}
         >
           <p>I&apos;m going to do nothing and live like a king</p>
-        </List>
+        </List> */}
       </main>
     );
   }
