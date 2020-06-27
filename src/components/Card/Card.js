8970e75ea1b08cefd0b0f20a41e0497cb9943430
olejos777 <olejniczakk.maciejj@gmail.com>
2020-06-27 21:09:55 +0200

    13.9 switch Card to function component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,23 +2,16 @@ import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
 
-class Card extends React.Component {
+const Card = props => (
+  <section className={styles.component}>
+    <h3 className={styles.title}>
+      {props.title}                          {/* it gets the title defined in List.js from objects <Column ... /> */}
+    </h3>
+  </section>
+);
 
-  static propTypes = {
-    title: PropTypes.node.isRequired,
-  };
-
-  render() {
-    const { title } = this.props;
-
-    return (
-      <section className={styles.component}>
-        <h3 className={styles.title}>
-          {this.props.title}                          {/* it gets the title defined in List.js from objects <Column ... /> */}
-        </h3>
-      </section>
-    );
-  }
-}
+Card.propTypes = {
+  title: PropTypes.node.isRequired,
+};
 
 export default Card;
