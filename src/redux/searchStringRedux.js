d33e843037ffafbf87f682a7cec9293dca511183
olejos777 <olejniczakk.maciejj@gmail.com>
2020-06-27 18:15:55 +0200

    13.5 add selectors for searchStringRedux

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,4 +1,10 @@
 // selectors
+export const getSearchString = ({ searchString }) => searchString;
+
+export const countVisibleCards = ({ cards, searchString }) =>
+  cards.filter(card => new RegExp(searchString, 'i').test(card.title));
+
+export const countAllCards = ({ cards }) => cards.length;
 
 // action name creator
 
